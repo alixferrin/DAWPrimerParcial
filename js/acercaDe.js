78468afd4189c0e3a7a4208ac1ba9beb7eeab0a2
gcmurillo <gcmurillo@outlook.com>
2017-11-18 16:28:34 -0500

    Pagina Acerca de

---
+++ js/acercaDe.js
@@ -0,0 +1,13 @@
+
+
+document.getElementById("botonGeancarlo").addEventListener("click", function(){
+	window.open("https://github.com/gcmurillo")
+})
+
+document.getElementById("botonAlix").addEventListener("click", function(){
+	window.open("https://github.com/alixferrin")
+})
+
+document.getElementById("botonJoe").addEventListener("click", function(){
+	window.open("https://github.com/JoeSvr95")
+})
